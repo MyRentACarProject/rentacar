9863bdb40b2e1f8f693703c63996d0e912942000
Ismet Turk <81475448+ismetturk@users.noreply.github.com>
2021-06-11 14:00:37 +0300

    Entities
    Entities klasörü yüklendi

---
+++ Entities/Concrete/Payment.cs
@@ -0,0 +1,19 @@
+﻿
+using Core.Entities;
+using System;
+
+namespace Entities.Concrete
+{
+    public class Payment : IEntity
+    {
+        public Payment()
+        {
+            PaymentDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int CustomerId { get; set; }
+        public int CardId { get; set; }
+        public decimal Total { get; set; }
+    }
+}
---
+++ Entities/DTOs/CarDetailsDto.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class CarDetailsDto:IDto
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public int ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+        public int FindexScore { get; set; }
+    }
+}
